13e489d0ddf8efa596134a2a93f13ebc92448806
Nickson-Simiyu <simiyunickson1@gmail.com>
2024-09-24 18:17:36 +0300

    fixed register

--- js/register.js
+++ js/register.js
@@ -28,6 +28,33 @@ document.addEventListener('DOMContentLoaded', function() {
     showStep(currentStep);
 });
 
+// Update the label based on slider value
+const roleSlider = document.getElementById('roleSlider');
+const roleLabel = document.getElementById('roleLabel');
+
+roleSlider.addEventListener('input', function() {
+    if (this.value == 1) {
+        roleLabel.textContent = "Parent";
+    } else if (this.value == 2) {
+        roleLabel.textContent = "Teacher";
+    } else if (this.value == 3) {
+        roleLabel.textContent = "Student";
+    }
+});
+
+// Redirect based on slider value
+function submitToggle() {
+    const selectedRole = document.querySelector('input[name="role"]:checked').value;
+
+    // Redirect based on the selected role
+    if (selectedRole === 'parent') {
+        window.location.href = 'register-parent.html';
+    } else if (selectedRole === 'teacher') {
+        window.location.href = 'register-teacher.html';
+    } else if (selectedRole === 'student') {
+        window.location.href = 'register-student.html';
+    }
+}
 
 
 /*let currentStep = 1;
